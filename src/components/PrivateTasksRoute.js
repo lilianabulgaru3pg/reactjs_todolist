5b9fd6cbd4fd1c8929ae415a279b4ca46aea154f
bliliana <lilianablg@gmail.com>
2018-12-03 15:00:01 +0200

    Create PrivateTasksRoute.js

---
+++ src/components/PrivateTasksRoute.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function PrivateTaskRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateTaskRoute;
