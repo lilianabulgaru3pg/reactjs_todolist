09df269cef7227c33f8717e21f881bd8d0f15ae8
bliliana <lilianablg@gmail.com>
2018-12-04 11:42:58 +0200

    Update Firewall.js

--- src/components/Firewall.js
+++ src/components/Firewall.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import { Firebase } from "../routes/firebaseConfig";
-import { withRouter } from "react-router-dom";
+import { Redirect, withRouter } from "react-router-dom";
 
 class Firewall extends Component {
   constructor(props) {
     super(props);
-    this.state = { redirectToReferrer: false };
     Firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
   }
 
   handleAuthStateChange = user => {
     console.log("AuthStateChanged", Firebase.auth().currentUser);
-    if (Firebase.auth().currentUser) {
-      this.setState({ redirectToReferrer: true });
-      this.props.history.push({ pathname: "/tasks", state: {} });
+    if (!Firebase.auth().currentUser) {
+      this.props.history.push({ pathname: "/login", state: {} });
+    } else {
+      this.props.history.replace({ pathname: "/tasks", state: {} });
     }
-    this.setState({ redirectToReferrer: false });
   };
 
   render() {
