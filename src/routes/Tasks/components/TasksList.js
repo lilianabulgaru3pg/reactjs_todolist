851ab13e0fdc37b4c38304a515e6c1cd46b05c28
bliliana <lilianablg@gmail.com>
2018-12-05 15:32:23 +0200

    Added update functionality on Task List

--- src/routes/Tasks/components/TasksList.js
+++ src/routes/Tasks/components/TasksList.js
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
+import { Firestore } from "../../../services/database";
+import { TASKS_COLLECTION } from "../../../constants";
+import { Auth } from "../../../services/firebaseConfig";
+import uuid from "uuid";
+
 class TasksList extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { tasks: [] };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    Firestore.collection(TASKS_COLLECTION)
+      .where("userID", "==", "1jY2WkbRKDQ2ENCamB4LEVBYbtw1")
+      .onSnapshot(docs => {
+        var newTasks = [];
+        docs.forEach(doc => {
+          newTasks.push(doc.data());
+        });
+        console.log("Current data: ", newTasks);
+        this.setState(tasks => ({ tasks: newTasks }));
+      });
+  }
   render() {
-    const items = [1, 2, 3, 4, 5];
-    const listItems = items.map((item, idx) => (
-      <li key={idx}>
-        <a href="">{item}</a>
+    let tasksList = this.state.tasks;
+    const listItems = tasksList.map(item => (
+      <li key={uuid()}>
+        <a href="">{item.name}</a>
       </li>
     ));
 
