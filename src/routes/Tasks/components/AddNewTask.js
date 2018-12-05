f02ff04a7b60fc7958754507cf05c9f2e9d40a5d
bliliana <lilianablg@gmail.com>
2018-12-05 16:38:17 +0200

    Create AddNewTask.js

---
+++ src/routes/Tasks/components/AddNewTask.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import InputField from "../../../components/InputField";
+import * as db from "../../../services/database";
+import { Auth } from "../../../services/firebaseConfig";
+import { TASKS_COLLECTION } from "../../../constants";
+
+class AddNewTask extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { taskInput: "" };
+  }
+
+  handleClick = event => {
+    let id = Auth.currentUser.uid;
+    let data = {
+      userID: id,
+      name: this.state.taskInput,
+      items: []
+    };
+    let response = db.postNewTaskData(TASKS_COLLECTION, data);
+    response
+      .then(docRef => {
+        console.log("Document written with ID: ", docRef.id);
+        this.setState({ taskInput: "" });
+      })
+      .catch(error => console.error("Error adding document: ", error));
+    event.preventDefault();
+  };
+
+  handleMouseOver = event => {
+    // todo change css
+    event.preventDefault();
+  };
+
+  handleInputChange = ev => {
+    this.setState({ taskInput: ev.target.value });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <InputField
+          className="add-task-input show-input"
+          onChange={this.handleInputChange}
+        />
+        <button
+          className="button-task button-style-1"
+          onClick={this.handleClick}
+          onMouseOver={this.handleMouseOver}
+        >
+          Add Task
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AddNewTask;
