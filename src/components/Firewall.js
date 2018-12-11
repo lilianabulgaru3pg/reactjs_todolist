f163e0563f1613599e8e4989365957620f40a27a
bliliana <lilianablg@gmail.com>
2018-12-11 14:19:37 +0200

    Update Firewall.js

--- src/components/Firewall.js
+++ src/components/Firewall.js
@@ -6,23 +6,30 @@ import { TASKS, LOGIN } from "../constants";
 class Firewall extends Component {
   constructor(props) {
     super(props);
-    Firebase.auth().onAuthStateChanged(this.handleAuthStateChange);
+    this.unsubscribe = Firebase.auth().onAuthStateChanged(
+      this.handleAuthStateChange
+    );
     this.state = { user: "", uid: "" };
   }
 
   handleAuthStateChange = user => {
     console.log("user.email", user);
+    var history = this.props.history;
     if (!user) {
       this.setState({ user: "", uid: "" });
-      this.props.history.push({ pathname: LOGIN, state: {} });
+      history.push({ pathname: LOGIN, state: {} });
     } else {
       this.setState({ user: user.email, uid: user.uid });
-      this.props.history.replace({
+      history.replace({
         pathname: TASKS,
         state: {}
       });
     }
   };
+
+  componentWillUnmount() {
+    if (this.unsubscribe) this.unsubscribe();
+  }
 
   render() {
     return (
