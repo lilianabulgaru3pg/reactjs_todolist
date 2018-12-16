fb33d8e156daa8a451c8c0cb4a9c2341e9ceba97
bliliana <lilianablg@gmail.com>
2018-12-16 22:57:11 +0200

    Added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,27 @@
 module.exports = {
-  extends: "airbnb",
-  plugins: ["prettier"],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  "parser": "babel-eslint",
+  "extends": ["airbnb", "plugin:jest/recommended","plugin:prettier/recommended", "prettier/flowtype", "prettier/react", "prettier/standard"],
+  "plugins": ["prettier", "jest"],
   rules: {
+    "strict": 0,
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "prettier/prettier": ["error", { singleQuote: true }]
+    "prettier/prettier": ["error", {"singleQuote": true, "parser": "flow"}],
+    "func-names": ["error", "never"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "comma-dangle": ["error", "never"],
+    "react/destructuring-assignment": ["<enabled>", 'never'],
+    "react/prop-types": 0,
+    "react/forbid-prop-types": 0,
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   }
 };
